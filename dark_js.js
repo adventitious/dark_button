ae1fcd5b06ed61d4f471be21e14cc30730090605
adventitious <47214398+adventitious@users.noreply.github.com>
2019-01-31 15:33:48 +0000

    Update dark_js.js

--- dark_js.js
+++ dark_js.js
@@ -3,7 +3,7 @@
 
 function toggleDarkLight() 
 {
-  console.log("qwe");
+  // console.log("qwe");
   var body = document.getElementById("body");
   var currentClass = body.className;
   body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
